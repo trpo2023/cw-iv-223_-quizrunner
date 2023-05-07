f8a84f64ca77c7600f8ac84028f5e18e2b535ec7
Efimov ghenya <IamRealYuki@users.noreply.github.com>
2023-05-07 22:52:32 +0700

    Counting time during test #7

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include "extract.h"
+
+double wtime()
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+}
 
 int main()
 {
@@ -30,7 +38,8 @@ int main()
     char *str3 = (char *)malloc(sizeof(char) * 200); // вариант ответа 3
     char *str4 = (char *)malloc(sizeof(char) * 200); // вариант ответа 4
 
-    printf("Тест начинается!\n");
+    printf("Тест начинается, ваше время пошло!\n");
+    double time_of_start = wtime();
     for (int i = 1; i <= n; i++)
     {
         printf("\n%s", get_question(i));
@@ -54,8 +63,10 @@ int main()
             printf("Ошибочка :<\n");
         }
     }
+    double time_of_end = wtime();
     double percent = (double)score / n * 100;
     printf("Ваш счет: %d из %d (%.2f%%)\n", score, n, percent);
+    printf("Время выполнения: %.2f секунд!\n", time_of_end - time_of_start);
     free(str1);
     free(str2);
     free(str3);
